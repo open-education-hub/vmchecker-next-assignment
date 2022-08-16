455c54bafed17c4ae3cb4164de20d19094b19275
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-08-17 00:49:28 +0200

    Add readme

--- src/main.c
+++ src/main.c
@@ -3,16 +3,5 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        fprintf(stderr, "Not enough arguments!\n");
-        return 1;
-    }
-
-    int number = atoi(argv[1]);
-
-    for (int i = 0; i < number; i++)
-        printf("1 ");
-
-    printf("\n");
     return 0;
 }
